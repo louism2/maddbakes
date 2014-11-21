cdd2515299bc2045efc47714899c47ae1361ff65
louism2 <louiscmancini@gmail.com>
2014-11-21 15:29:33 -0800

    removed cookie test fixture for feature specs

--- spec/factories/test_data.rb
+++
@@ -1,9 +0,0 @@
-class TestData
-  
-  def self.create_user_cookie(user, request)
-    jar = ActionDispatch::Cookies::CookieJar.build(request)
-    jar.signed[:head_bitch] = [user.id, user.salt]
-    request.cookies['head_bitch'] = jar[:head_bitch]
-  end
-  
-end
