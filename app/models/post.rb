93ce49d84911e3ada800f61297ec63d0c1a4aa9a
louism2 <louiscmancini@gmail.com>
2014-11-21 16:14:23 -0800

    tests for post validations passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,10 @@ class Post < ActiveRecord::Base
   has_many :photos
   validates_associated :photos
 
+  validates :comment,   presence: true,
+                        length: {maximum: 300}
+                        
+  validates :author,    length: {maximum: 50}                      
+
 
 end
