1f5e731cc624e653597ce7f5a2edb5a97d714e7d
louism2 <louiscmancini@gmail.com>
2014-12-09 10:12:02 -0800

    added save method with db constraint error handling, delegated to Post class

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,18 @@
 class Photo < ActiveRecord::Base
   
   belongs_to :post
+  delegate :save, to: :post
   
   has_attached_file :image_file
   
   validates_attachment_content_type :image_file, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   
   
-  
+  def save(*args)
+    super
+  rescue ActiveRecord::RecordNotUnique => error
+    errors[:base] << "You can only have one photo be your header photo.  Try your selection again"
+    false
+  end
   
 end
