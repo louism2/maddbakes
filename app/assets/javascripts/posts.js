f0e4709ff1cb8453c865a6683beba755444e5384
louism2 <louiscmancini@gmail.com>
2014-12-10 11:36:30 -0800

    removing file field from post form spec passing

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,5 +1,4 @@
 $(function() {
-	console.log('here')
 	if($('#post_form').length > 0){
 		setAddFileListener();
 		setRemoveFileListener();
@@ -7,15 +6,28 @@ $(function() {
 });
 
 function setAddFileListener(){
-	var $file_field = $("input[type='file']");
+	var $file_field = $(".file_field_container");
 	$('#add_file_field').on('click', function(){
 		var $cloned_file_field = $file_field.clone();
 		var timestamp = new Date().getTime();
-		$cloned_file_field.attr('name', 'post[photos_attributes]['+timestamp+'][image_file]');
+		$cloned_file_field.find('input').attr('name', 'post[photos_attributes]['+timestamp+'][image_file]');
 		$(this).before($cloned_file_field);
+		setRemoveFileListener();
+		return false;
 	});
 }
 
 function setRemoveFileListener(){
-	
+	var $file_fields = $('input[type="file"]');
+	$('.remove_file').on('click', function(e){
+		e.preventDefault();
+		var $file_container = $(this).parent();
+		if($file_fields.length == 1){
+			$file_container.replaceWith($file_container.clone());
+		}else{
+			$file_container.remove();
+		}
+		setRemoveFileListener();
+		return false;
+	});
 }
--- spec/javascripts/view_spec.js
+++ spec/javascripts/view_spec.js
@@ -1,14 +1,51 @@
 describe('Post Form', function(){
   
-  describe('adding another file to the form', function(){
+	describe('adding another file to the form', function(){
     
-    it('should add a file field when clicking "add file field"', function(){
-    	loadFixtures('post_form.html');
-		setAddFileListener();
-		$('#add_file_field').click();
-		expect($('input[type="file"]').length).toEqual(2);
-	});
+	    it('should add a file field when clicking "add file field"', function(){
+	    	loadFixtures('post_form_one_file.html');
+			setAddFileListener();
+			$('#add_file_field').click();
+			expect($('input[type="file"]').length).toEqual(2);
+		});
+	
+		it('file fields should have different names', function(){
+		    loadFixtures('post_form_one_file.html');
+			setAddFileListener();
+			$('#add_file_field').click();
+			var $file_fields = $('input[type="file"]');
+			expect($file_fields.eq(0).attr('name')).not.toEqual($file_fields.eq(1).attr('name'));		
+		});
   
-  });
+  	});
+
+	describe('removing a file field', function(){
+		describe('more than one file field present', function(){
+			
+			it('removes the file field', function(){
+				loadFixtures('post_form_two_files.html');	
+				setRemoveFileListener();				
+				$('.remove_file').eq(1).click();
+				expect($('input[type="file"]').length).toEqual(1);
+			});
+			
+		});
+		
+		describe('only one file field present', function(){
+			
+			it('replaces the file_field', function(){
+				loadFixtures('post_form_one_file.html');	
+				var $beforeRemove = $('input[type="file"]');
+				setRemoveFileListener();				
+				$('.remove_file').click();
+				var $afterRemove = $('input[type="file"]');
+				expect($afterRemove).toBeInDOM();
+				expect($afterRemove).not.toEqual($beforeRemove);
+			});
+			
+		})
+	});
+
+	
 
 });  
