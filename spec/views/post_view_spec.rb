bff123c837499e3866f7ab12bec39548785d941a
louism2 <louiscmancini@gmail.com>
2014-12-10 16:20:55 -0800

    displaying list of photos view spec passing

--- spec/views/post_view_spec.rb
+++ spec/views/post_view_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 require_relative '../fixtures/test_data.rb'
 
 describe 'posts/new.html.erb' do 
+  
   context 'when more than one header photo is selected' do
     it "displays an error on the form" do 
       assign(:post, Post.create(TestData::POST_WITH_MULTIPLE_HEADER_PHOTOS))
@@ -9,4 +10,24 @@ describe 'posts/new.html.erb' do
       rendered.should have_content('You can only have one photo be your header photo')
     end
   end
+  
+end
+
+describe 'posts/edit.html.erb' do
+  
+  context 'when a post has photos' do
+    
+    before(:each) do
+      FactoryGirl.create(:post_with_photos)
+      @post = Post.last
+    end
+    
+    it 'displays the list of photos' do
+      assign(:post, @post)
+      render
+      page = Capybara::Node::Simple.new( rendered )
+      expect(page.has_css?('img.thumbnail', :count => 2)).to be(true)
+    end
+  end
+  
 end
