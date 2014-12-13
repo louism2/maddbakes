4c479eaf253700772aba0f0ab70639d17cb92ae0
louism2 <louiscmancini@gmail.com>
2014-12-12 18:56:31 -0800

    front-end destroying comments and posts specs failing

---
+++ spec/views/delete_posts_and_comments_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+require 'webmock/rspec'
+include WebmockStubs
+include Session
+
+describe 'posts/show.html.erb' do
+  
+  context 'a signed in user' do
+    
+    before(:each) do
+      allow(view).to receive(:signed_in?).and_return(true)
+      stub_s3_request
+      FactoryGirl.create(:post_with_photos_and_comments)
+    end
+    
+    
+    it 'shows a link to delete the post' do
+      assign(:post, Post.last)
+      assign(:comment, Comment.new)
+      render
+      page = Capybara::Node::Simple.new(rendered)
+      expect(page.has_css?('.destroy_post')).to eq(true)
+    end
+    
+    it 'shows a link to delete a comment' do
+      assign(:post, Post.last)
+      assign(:comment, Comment.new)
+      render
+      page = Capybara::Node::Simple.new(rendered)
+      expect(page.has_css?('.destroy_comment')).to eq(true)
+    end
+    
+  end
+    
+end
