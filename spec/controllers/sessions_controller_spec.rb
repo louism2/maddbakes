4ba54584499c5a9c6fcfcaf583d47e6829bd6c46
louism2 <louiscmancini@gmail.com>
2014-12-15 19:24:57 -0800

    SessionsController#create with valid data failing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe SessionsController, :type => :controller do
+describe SessionsController do
+
+  describe '#create' do
+    
+    context 'a valid email and password' do
+      
+      let(:user){ FactoryGirl.create(:new_user) }
+      
+      it 'sets the session cookie' do
+        post :create, session: {email: user.email, password: user.password}
+        expect(@response.cookies[:head_bitch]).not_to be_nil
+      end    
+    
+      it 'redirects to the homepage' do 
+        post :create, session: {email: user.email, password: user.password}
+        expect(@response).to redirect_to(root_path)
+      end 
+      
+    end
+    
+  end
 
 end
