2d577e64013fb162fe23ad0de9c66019b089c4de
louism2 <louiscmancini@gmail.com>
2015-01-09 20:47:33 -0800

    added cover_photo column to photos table

--- db/migrate/20141202181005_add_header_image_to_photos.rb
+++ db/migrate/20141202181005_add_header_image_to_photos.rb
@@ -1,5 +1,6 @@
 class AddHeaderImageToPhotos < ActiveRecord::Migration
   def change
     add_column :photos, :header_photo, :boolean, default: false
+    add_column :photos, :cover_photo, :boolean, default: false    
   end
 end
