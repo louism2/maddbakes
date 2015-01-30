05c20716aa224e16513e585c69b12671110d11eb
louism2 <louiscmancini@gmail.com>
2015-01-30 10:43:50 -0800

    updated edit_photos form to conditonally wrap thumbnails in form tag

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -4,8 +4,12 @@ module PhotosHelper
     "https://maddbakes.s3.amazonaws.com/photos/#{id}/original.jpg"
   end  
   
-  def image_alteration_banner
-    "<p align='center' id='image_alteration_banner'>You have changes pending.  Click the button below to submit your changes<br><button id='confirm_photo_changes_button'>Confirm Changes</button></p>".html_safe
+  def opening_form_tag_for_photos
+    "<form action='/photos/update_images' />".html_safe
+  end 
+  
+  def closing_form_tag_and_submit_button
+    "<button type='submit' />Update Photos</button></form>".html_safe
   end  
   
 end
