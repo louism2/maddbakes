4f2a98b1e00cde8286de713f4f647edf862b3425
louism2 <louiscmancini@gmail.com>
2015-01-29 18:42:38 -0800

    updated specs to remove 'edit_photos' form and replaced with manual photo_id submittal

---
+++ spec/javascripts/edit_photos_spec.js
@@ -0,0 +1,41 @@
+describe('Edit Photos', function(){
+	
+	describe('updatedEditedPhotos()', function(){
+	  describe('deleting photos', function(){
+	   describe('when the photo isn\'t currently in the list', function(){
+	     it('the photo id is added to the "delete" property of the "editedPhotos" object', function(){
+	     	
+	     });	
+	   })
+		 describe('when the photo is already in the list', function(){
+	     it('the photo id is removed to the "delete" property of the "editedPhotos" object', function(){
+	     	
+	     });			 	
+		 });	
+	  });
+		
+		
+		
+		
+		
+		
+		describe('cover photos', function(){
+ 	   describe('when the photo isn\'t currently in the list', function(){
+	   	
+ 	   })
+ 		 describe('when the photo is already in the list', function(){
+		 	
+ 		 });				
+		});
+		
+		describe('header photos', function(){
+ 	   describe('when the photo isn\'t currently in the list', function(){
+	   	
+ 	   })
+ 		 describe('when the photo is already in the list', function(){
+		 	
+ 		 });				
+		});	
+	});
+	
+});	
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -19,14 +18,22 @@ require 'capybara/rspec'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
+
+# require all files in spec/support
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+#require all files in spec/fixtures
+Dir[Rails.root.join("spec/fixtures/**/*.rb")].each { |f| require f }
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
