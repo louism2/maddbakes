5590eca6d7cb8a91a0641fc68ef5dc52c8fface4
louism2 <louiscmancini@gmail.com>
2015-01-29 18:47:06 -0800

    added photo banner that alerts users to changes in their photos while editing

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -3,4 +3,9 @@ module PhotosHelper
   def build_s3_url(id)
     "https://maddbakes.s3.amazonaws.com/photos/#{id}/original.jpg"
   end  
+  
+  def image_alteration_banner
+    "<p align='center' id='image_alteration_banner'>You have changes pending.  Click the button below to submit your changes<br><button id='confirm_photo_changes_button'>Confirm Changes</button></p>".html_safe
+  end  
+  
 end
